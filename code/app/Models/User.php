71f1360353518dc5d721556f66f0e2b5ffd43272
Bryce Meyer <bryce@pomeloproductions.com>
2017-06-02 18:57:37 -0500

    described user

--- code/app/Models/User.php
+++ code/app/Models/User.php
@@ -5,6 +5,15 @@ namespace Sublet\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * Class User
+ * @package Sublet\Models
+ *
+ * @property $id int the unique id in the database
+ * @property $email string unique to a single user
+ * @property $name string the real name of the user
+ * @property $password string the hashed password of this user
+ */
 class User extends Authenticatable
 {
     use Notifiable;
