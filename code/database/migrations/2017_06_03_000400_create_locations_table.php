1d424f0553f92056c6d0fe1ded53e53fb95f62ed
Bryce Meyer <bryce@pomeloproductions.com>
2017-06-02 19:07:03 -0500

    added locations migration

---
+++ code/database/migrations/2017_06_03_000400_create_locations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->string('address_line_1');
+            $table->string('address_line_2')->nullable();
+            $table->string('postal_code');
+            $table->string('city');
+            $table->string('state');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
