7399b3d1c9ca918990a61a82768c2a445724ce20
Bryce Meyer <bryce@pomeloproductions.com>
2017-06-02 19:42:27 -0500

    added migration for reviews table

---
+++ code/database/migrations/2017_06_03_003106_create_reviews_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('content');
+            $table->string('private_message');
+            $table->double('score');
+            $table->unsignedInteger('request_id')->references('id')->on('requests');
+            $table->unsignedInteger('user_id')->references('id')->on('users');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
